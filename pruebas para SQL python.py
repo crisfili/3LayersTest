8fb9ee5b5e583b2cfc7efef73d5222fa648f2cf2
crisfili <114680711+crisfili@users.noreply.github.com>
2023-01-05 18:07:32 -0300

    Delete pruebas para SQL python.py

--- pruebas para SQL python.py
+++
@@ -1,9 +0,0 @@
-cursor.execute("CREATE USER (%s) PASSWORD (%s)", ("user", "default_password",))
-sql = "CREATE USER %s PASSWORD %s;"
-
-
-cursor.execute(sql.format('{user}', "{default_password}",))
-
-cursor.execute("CREATE USER %s PASSWORD %s", ["user", "default_password"])
-
-cursor.execute("CREATE USER %s PASSWORD %s", ("user", "default_password"))
